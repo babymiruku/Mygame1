5d131a0e478523964ba02661ff73cf986fef78fc
DESKTOP-8U651S1\User <amonwan161@gmail.com>
2021-09-02 12:54:34 +0700

    homework1

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    int n;
+    void Start()
+    {
+        for (int i = 1; i <= 100; i++) 
+        {
+            print(i);
+        }
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+                print(i);
+        }
+        float x = 1.0f;
+        float r = circle(x);
+        //print(r);
+
+
+    }
+     float circle(float x)
+    {
+        float result;
+        x = 5.0f;
+        result = 3.14f * (x*x) ;
+        print(result);
+        return result;
+    }
+}
